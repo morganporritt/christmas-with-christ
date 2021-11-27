554c871fc7649c2de472b7e58618a4113b6dc68e
mporritt <mporritt@integrafinancialservice.com>
2021-11-26 23:17:43 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+      class="w-full bg-center bg-cover"
+      :style="{ backgroundImage: 'url(./assets/background.png)' }"
+      style='height: calc(100vh - 3rem);'
+  >
+  </div>
+
+
+<!--    <img-->
+<!--        src="./assets/background.png"-->
+<!--        class="h-screen bg-center bg-cover"-->
+<!--    >-->
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      bgImage: "./assets/background.png"
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
