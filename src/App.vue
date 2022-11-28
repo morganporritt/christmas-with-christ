6b9a0419a0cf431ef9c84c8c7bbccf944ab3de4b
morganporritt <morganporritt@gmail.com>
2022-11-28 14:49:58 -0700

    Updated banner.

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
   <div>
     <div class="relative">
       <div
-          class="page_banner_image w-full bg-center bg-cover"
-          :style="{ backgroundImage: 'url(' + require('@/assets/images/background.png') + ')' }"
-          style='height: calc(100vh - 3rem);'
+        class="page_banner_image w-full bg-center bg-cover"
+        :style="{
+          backgroundImage:
+            'url(' + require('@/assets/images/background.png') + ')',
+        }"
+        style="height: calc(100vh - 3rem)"
       >
-        <div class="absolute pr-3 text-center text-gray-50 top-24 right-0.5 md:right-4 w-max">
-          <h1 class="text-xl sm:text-5xl lg:text-6xl xl:text-7xl">Christmas</h1>
+        <div
+          class="absolute pr-3 text-center text-gray-50 top-24 left-0.5 md:left-4 w-max"
+        >
+          <h1 class="text-xl sm:text-5xl lg:text-6xl xl:text-7xl">
+            Christmas
+          </h1>
           <h1 class="text-xs sm:text-base lg:text-lg">with</h1>
-          <h1 class="text-xl sm:text-5xl lg:text-6xl xl:text-7xl">Christ</h1>
-
+          <h1 class="text-xl sm:text-5xl lg:text-6xl xl:text-7xl">
+            Christ
+          </h1>
         </div>
       </div>
     </div>
@@ -20,23 +28,23 @@
 </template>
 
 <script>
-import Navigation from "@/components/Navigation/Navigation";
+import Navigation from '@/components/Navigation/Navigation';
 export default {
-  components: {Navigation}
-}
+  components: { Navigation },
+};
 </script>
 
 <style>
 @font-face {
-  font-family: "TrajanRegular";
-  src: local("TrajanRegular"),
-  url(./assets/fonts/TrajanPro3Regular.ttf) format("truetype");
+  font-family: 'TrajanRegular';
+  src: local('TrajanRegular'),
+    url(./assets/fonts/TrajanPro3Regular.ttf) format('truetype');
 }
 
 @font-face {
-  font-family: "TrajanBold";
-  src: local("TrajanBold"),
-  url(./assets/fonts/TrajanPro3Bold.ttf) format("truetype");
+  font-family: 'TrajanBold';
+  src: local('TrajanBold'),
+    url(./assets/fonts/TrajanPro3Bold.ttf) format('truetype');
 }
 
 h1 {
@@ -46,5 +54,4 @@ h1 {
 h2 {
   font-family: 'TrajanBold', serif;
 }
-
 </style>
