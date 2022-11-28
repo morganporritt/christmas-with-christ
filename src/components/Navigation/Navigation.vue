24f05338842959f93c4a943e43289d87459f9cfe
morganporritt <morganporritt@gmail.com>
2022-11-28 14:37:29 -0700

    Removed the more nav

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,47 +1,45 @@
 <template>
-    <div class="Navigation">
-      <div id="nav" class="absolute top-0 z-10 bg-gray-100 bg-opacity-10 w-full flex flex-row justify-center py-3">
-
-        <div
-            v-for="item in navigationItems"
-            v-bind:key="item.id"
+  <div class="Navigation">
+    <div
+      id="nav"
+      class="absolute top-0 z-10 w-full flex flex-row justify-end py-3"
+    >
+      <div v-for="item in navigationItems" v-bind:key="item.id">
+        <router-link
+          :to="{ path: item.href }"
+          class="text-gray-50 px-3 hover:text-gray-500 sm:text-lg sm:px-5"
         >
-          <router-link
-              :to="{path:item.href}"
-              class="text-gray-50 px-3 hover:text-gray-500 sm:text-lg sm:px-5"
-          >
-            {{item.text}}
-          </router-link>
-        </div>
+          {{ item.text }}
+        </router-link>
       </div>
-      <router-view/>
     </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Navigation',
-        data ()
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      navigationItems: [
         {
-          return {
-            navigationItems: [
-              {
-                id: 1,
-                text: 'Home',
-                href: '/',
-              },
-              {
-                id: 2,
-                text: 'Quotes',
-                href: '/quotes',
-              },
-              {
-                id: 3,
-                text: 'More',
-                href: '/more',
-              },
-            ]
-          }
-        }
+          id: 1,
+          text: 'Home',
+          href: '/',
+        },
+        {
+          id: 2,
+          text: 'Quotes',
+          href: '/quotes',
+        },
+        // {
+        //   id: 3,
+        //   text: 'More',
+        //   href: '/more',
+        // },
+      ],
     };
+  },
+};
 </script>
